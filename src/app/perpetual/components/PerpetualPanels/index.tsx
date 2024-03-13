802741f6814772176ab6ab0360d374dca1182b6d
Lena <Lena_git6@protonmail.com>
2024-03-13 15:07:05 +0800

    tab

--- src/app/perpetual/components/PerpetualPanels/index.tsx
+++ src/app/perpetual/components/PerpetualPanels/index.tsx
@@ -2,97 +2,83 @@
 import styled from "styled-components";
 import DraggableIcon from "../DraggableIcon";
 import { useState } from "react";
+import Tabs from "../Tabs";
+import { tabProps } from "../Tabs";
 
-type ActionTabsProps = {
-  activeTab: string;
-};
 const Wrapper = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
 `;
-const ActionTabs = styled.div<ActionTabsProps>`
+const ActionTabs = styled(Tabs)`
   width: 100%;
   display: flex;
   align-items: center;
   position: relative;
+  gap: 0;
   &::before {
-    width: 50%;
-    height: 2px;
-    background-color: ${(props) => props.theme.colors.primary1};
-    content: "";
-    position: absolute;
     top: 0;
-    transition: all 0.3s linear;
-    left: ${(props) => {
-      return props.activeTab === "open" ? "0" : "50%";
-    }};
+    z-index: 4;
   }
   .tab {
-    box-sizing: border-box;
     height: 41px;
     flex: 1;
-    display: flex;
-    align-items: center;
     justify-content: center;
     color: ${(props) => props.theme.colors.text1};
-    font-family: Arial;
     font-size: ${(props) => props.theme.fontSize.reguar};
-    font-style: normal;
-    font-weight: 400;
-    line-height: 100%;
     background-color: ${(props) => props.theme.colors.fill2};
-    cursor: pointer;
-    transition: all 0.3s linear;
+
     &:hover {
       background-color: ${(props) => props.theme.colors.fill1};
     }
   }
-  .active {
+  .active_tab {
     background-color: ${(props) => props.theme.colors.fill1};
     color: ${(props) => props.theme.colors.primary1};
   }
 `;
+
 const Content = styled.div`
   padding: 0 14px 18px 14px;
 `;
-const OrderTypeTabs = styled.div`
-  display: flex;
-  align-items: center;
-  border-bottom: ${(props) => `1px solid ${props.theme.colors.border1}`};
+
+const OrderTypeTabs = styled(Tabs)`
   padding: 10px 0 5px 0;
   .tab {
-    color: ${(props) => props.theme.colors.text4};
-    font-family: Arial;
-    font-size: ${(props) => props.theme.fontSize.medium};
-    font-style: normal;
-    font-weight: 400;
-    line-height: 100%;
+    padding: 0px 8px 5px 0px;
   }
 `;
 const PerpetualPanels = () => {
-  const [activeTab, setActiveTab] = useState("open");
+  const [activeTab, setActiveTab] = useState<string>("open");
+  const [activeOrderTab, setActiveOrderTab] = useState<string>("limit");
+
   const actionTabList = [
     { label: "OPEN", key: "open" },
     { label: "CLOSE", key: "close" },
   ];
+  const orderTypeTabList = [
+    { label: "Limit", key: "limit" },
+    { label: "Market", key: "market" },
+  ];
+
   return (
     <Wrapper>
-      <ActionTabs activeTab={activeTab}>
-        {actionTabList.map((i) => {
-          return (
-            <div
-              onClick={() => setActiveTab(i?.key)}
-              key={i?.key}
-              className={`tab ${activeTab === i?.key ? "active" : ""}`}
-            >
-              {i?.label}
-            </div>
-          );
-        })}
-      </ActionTabs>
+      <ActionTabs
+        gap={0}
+        list={actionTabList}
+        handleClick={(item: tabProps) => {
+          setActiveTab(item?.key);
+        }}
+      />
+
       <Content>
-        <OrderTypeTabs></OrderTypeTabs>
+        <OrderTypeTabs
+          gap={20}
+          list={orderTypeTabList}
+          handleClick={(item: tabProps) => {
+            setActiveOrderTab(item?.key);
+          }}
+        />
       </Content>
       <DraggableIcon />
     </Wrapper>
