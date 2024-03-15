cda96125aa7151f9ccda58744d8eb23d5de4ebbf
WangXin <axin-free@outlook.com>
2024-03-15 14:35:35 +0800

    feat: stake pool page ui

---
+++ src/app/stake/components/Pool/CurrentEpochContent.tsx
@@ -0,0 +1,149 @@
+import { motion } from "framer-motion"
+import styled from "styled-components"
+
+const Fields = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const FieldColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 26px;
+`
+
+const FieldItem = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  column-gap: 20px;
+  border: 1px solid transparent;
+  padding: 8px 17px 8px 14px;
+  border-radius: 8px;
+`
+
+const FieldAnimateItem = styled.div`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  border: 1px solid transparent;
+  padding: 8px 17px 8px 14px;
+  border-radius: 8px;
+  cursor: pointer;
+  &:hover {
+    border-color: ${props => props.theme.colors.primary1};
+    background-color: ${props => props.theme.colors.fill3};
+
+    .label {
+      color: ${props => props.theme.colors.text1};
+    }
+
+    .value {
+      color: ${props => props.theme.colors.primary1};
+    }
+  }
+`
+
+const FieldLabel= styled.div`
+  color: ${props => props.theme.colors.text4};
+  font-size: ${props => props.theme.fontSize.small};
+`
+
+const FieldTime = styled.div`
+  color: ${props => props.theme.colors.text1};
+  font-size: ${props => props.theme.fontSize.small};
+`
+
+const FieldValue = styled.div`
+  color: ${props => props.theme.colors.text1};
+  font-size: ${props => props.theme.fontSize.header2};
+`
+
+const EpochTitle = styled.h2`
+  font-size: ${props => props.theme.fontSize.header0};
+`
+
+const Divider = styled.div`
+  width: 1px;
+  background-color: ${props => props.theme.colors.border1};
+  height: 75px;
+`
+
+const token = 'QLP'
+export function CurrentEpochContent () {
+  const epochFieldsLeft = [
+    {
+      key: 'start-in',
+      label: "Start in",
+      value: "2024-2-26 00:00",
+    },
+    {
+      key: 'end-in',
+      label: "End in",
+      value: "2024-2-26 00:00",
+    }
+  ]
+  const epochFieldsMiddle = [
+    {
+      key: 'total',
+      label: `Total ${token}`,
+      value: "123,123.00",
+    },
+    {
+      key: 'TVL',
+      label: "TVL",
+      value: "$123,123,123.00",
+    }
+  ]
+  const epochFieldsRight = [
+    {
+      key: 'your-position',
+      label: "Your Position",
+      value: "123,123.00 QLP",
+    },
+    {
+      key: 'liquidity-supplied',
+      label: "Liquidity Supplied",
+      value: "$123,123.00",
+    }
+  ]
+  return (
+    <>
+      <EpochTitle>Current Epoch 100</EpochTitle>
+      <Fields>
+        <FieldColumn>
+          {
+            epochFieldsLeft.map(field => (
+              <FieldItem key={field.key}>
+                <FieldLabel>{field.label}</FieldLabel>
+                <FieldTime>{field.value}</FieldTime>
+              </FieldItem>
+            ))
+          }
+        </FieldColumn>
+        <Divider />
+        <FieldColumn>
+          {
+            epochFieldsMiddle.map(field => (
+              <FieldAnimateItem key={field.key}>
+                <FieldLabel className="label">{field.label}</FieldLabel>
+                <FieldValue className="value">{field.value}</FieldValue>
+              </FieldAnimateItem>
+            ))
+          }
+        </FieldColumn>
+        <Divider />
+        <FieldColumn>
+          {
+            epochFieldsRight.map(field => (
+              <FieldAnimateItem key={field.key}>
+                <FieldLabel className="label">{field.label}</FieldLabel>
+                <FieldValue className="value">{field.value}</FieldValue>
+              </FieldAnimateItem>
+            ))
+          }
+        </FieldColumn>
+      </Fields>
+    </>
+  )
+}
---
+++ src/app/stake/components/Pool/index.tsx
@@ -0,0 +1,63 @@
+import styled from "styled-components"
+import { CurrentEpochContent } from "./CurrentEpochContent"
+import { AssetsContent } from "./AssetsContent"
+import { Chart } from "./Chart"
+import { BuySellContent } from "./BuySellContent"
+
+const Wrapper = styled.div`
+  padding-top: 20px;
+  display: grid;
+  grid-template-columns: 1fr 510px;
+  grid-template-rows: 200px 220px 170px;
+  gap: 10px;
+  grid-template-areas: 
+    "current-epoch buy-sell"
+    "price-chart buy-sell"
+    "price-chart assets";
+`
+
+const Box = styled.div`
+  background-color: ${props => props.theme.colors.fill2};
+  border-radius: 8px;
+`
+
+const CurrentEpoch = styled(Box)`
+  grid-area: current-epoch;
+  padding: 26px 28px 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const BuySellPanel = styled.div`
+  grid-area: buy-sell;
+  background-color: ${props => props.theme.colors.fill2};
+  border-radius: 0 0 8px 8px;
+`
+
+const PriceChart = styled(Box)`
+  grid-area: price-chart;
+  padding: 23px 28px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`
+
+const Assets = styled(Box)`
+  grid-area: assets;
+  padding: 21px 35px;
+`
+
+function PoolContent () {
+  
+  return (
+    <Wrapper>
+      <CurrentEpoch><CurrentEpochContent /></CurrentEpoch>
+      <BuySellPanel><BuySellContent /></BuySellPanel>
+      <PriceChart><Chart /></PriceChart>
+      <Assets><AssetsContent /></Assets>
+    </Wrapper>
+  )
+}
+
+export default PoolContent
